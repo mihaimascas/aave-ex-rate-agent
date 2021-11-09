4cff1d2f55ab66d27f32982e084984bb8e08ece6
Mihai Mascas <mihai.mascas@cardoplus.ro>
2021-11-09 02:20:25 +0200

    AAVE Exchange Rates agent

--- README.md
+++ README.md
@@ -1,27 +1,53 @@
-# High Gas Agent
+# Forta Agent: AAVE Exchange Rates
 
 ## Description
 
-This agent detects transactions with high gas consumption
+This agent detects changes in exchange rates for given pare of tokens.
+
+Environment variables (.env) are used to set up the agent. the following settings can be applied:
+- Tokens pair
+- alerts thresholds that drive the severity of the finding
+- alert for exchange going down or/and up
+- MainNet address 
 
 ## Supported Chains
 
 - Ethereum
-- List any other chains this agent can support e.g. BSC
 
 ## Alerts
 
-Describe each of the type of alerts fired by this agent
+- FORTA-AAVE_EXR_DOWN
+  - Fired when exchange rate goes down 
+  - Severity: 
+    - "info": first thresholds is reached
+    - "medium": 2nd thresholds is reached
+    - "high": 3rd thresholds is reached
+    - "critical": 4th thresholds is reached
+  - Type is always set to "info"
+  - Meta Data:
+    - 'diff': exchange rate difference
+    - 'token1',
+    - 'token2',
+    - 'currentExRate',
+    - 'previousExRate',
 
-- FORTA-1
-  - Fired when a transaction consumes more gas than 1,000,000 gas
-  - Severity is always set to "medium" (mention any conditions where it could be something else)
-  - Type is always set to "suspicious" (mention any conditions where it could be something else)
-  - Mention any other type of metadata fields included with this alert
+- FORTA-AAVE_EXR_UP
+  - Fired when exchange rate goes up
+  - Severity:
+    - "info": first thresholds is reached
+    - "medium": 2nd thresholds is reached
+    - "high": 3rd thresholds is reached
+    - "critical": 4th thresholds is reached
+  - Type is always set to "info"
+  - Meta Data:
+    - 'diff': exchange rate difference
+    - 'token1',
+    - 'token2',
+    - 'currentExRate',
+    - 'previousExRate',
 
 ## Test Data
 
 The agent behaviour can be verified with the following transactions:
 
-- 0x1b71dcc24657989f920d627c7768f545d70fcb861c9a05824f7f5d056968aeee (1,094,700 gas)
-- 0x8df0579bf65e859f87c45b485b8f1879c56bc818043c3a0d6870c410b5013266 (2,348,226 gas)
+TDB
--- src/agent.ts
+++ src/agent.ts
@@ -1,45 +1,119 @@
 import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
+import {
+  BlockEvent,
+  Finding,
+  FindingSeverity,
+  FindingType,
+  getJsonRpcUrl,
+  HandleBlock,
 } from 'forta-agent'
+import Web3 from 'web3';
+import {AbiItem} from 'web3-utils'
+import * as dotenv from 'dotenv';
+import tokens from './tokens.json';
+import landingPoolAbiItems from './abi/landing-pool.json';
+import assetPriceAbiItem from './abi/asset-price.json';
 
-let findingsCount = 0
+dotenv.config();
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+let previousExRate: BigNumber;
+const web3 = new Web3(getJsonRpcUrl());
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+const token1 = tokens.find((t) => t.symbol === process.env.TOKEN_1);
+const token2 = tokens.find((t) => t.symbol === process.env.TOKEN_2);
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
-    findingsCount++
+const alertType = process.env.ALERT_DEVIATION || 'UP,DOWN';
+const thresholds: number[] = [
+    parseInt(process.env.EXR_DEVIATION_PROCENT_INFO || '0.005', 10),
+    parseInt(process.env.EXR_DEVIATION_PROCENT_MEDIUM || '0.01', 10),
+    parseInt(process.env.EXR_DEVIATION_PROCENT_HIGH || '0.05', 10),
+    parseInt(process.env.EXR_DEVIATION_PROCENT_CRITICAL || '0.1', 10),
+];
+
+const generateFindings = (exRate: BigNumber): Finding[] => {
+  const findings: Finding[] = [];
+
+  if (!previousExRate || !token1 || !token2) {
+    return findings;
   }
 
-  return findings
+  const dif = previousExRate.minus(exRate);
+  const type = dif.isGreaterThan(0)
+      ? 'DOWN'
+      : dif.isLessThan(0)
+          ? 'UP'
+          : null;
+
+  if (!type || dif.isLessThan(thresholds[0]) || !alertType.includes(type)) {
+    return findings;
+  }
+
+  let severity: FindingSeverity = FindingSeverity.Info;
+
+  switch (true) {
+    case dif.abs().isGreaterThanOrEqualTo(thresholds[3]):
+      severity = FindingSeverity.Critical;
+      break;
+    case dif.abs().isGreaterThanOrEqualTo(thresholds[2]):
+      severity = FindingSeverity.High;
+      break;
+    case dif.abs().isGreaterThanOrEqualTo(thresholds[1]):
+      severity = FindingSeverity.Medium;
+      break;
+  }
+
+  findings.push(Finding.fromObject({
+    name: `Exchange rate ${type} ${token1?.symbol}/${token2?.symbol}`,
+    description: `Exchange rate ${type} by ${dif.dividedBy(100).toFormat(2)}% for ${token1?.symbol}/${token2?.symbol}`,
+    alertId: `FORTA-AAVE_EXR_${type}`,
+    severity,
+    type: FindingType.Info,
+    metadata: {
+      'diff': dif.toFormat(4),
+      'token1': token1.symbol,
+      'token2': token2.symbol,
+      'currentExRate': exRate.toFormat(4),
+      'previousExRate': previousExRate.toFormat(4),
+    }
+  }));
+
+  return findings;
 }
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+let priceOracleAddress: string;
+
+const getPriceOracleAddress = async () => {
+  if (priceOracleAddress) {
+    return priceOracleAddress;
+  }
+
+  const poContract = new web3.eth.Contract(landingPoolAbiItems as AbiItem[], process.env.LANDING_POOL_ADDRESS);
+  priceOracleAddress = await poContract.methods.getPriceOracle().call();
+
+  return priceOracleAddress;
+}
+
+const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+  const findings: Finding[] = [];
+
+  if (!token1 || !token2) {
+    return findings;
+  }
+
+  const poAddress = await getPriceOracleAddress();
+
+  const exrContract = new web3.eth.Contract(assetPriceAbiItem as AbiItem, poAddress);
+  const [priceToken1, priceToken2] = await exrContract.methods.getAssetsPrices({
+    assets: [token1.address, token2.address]
+  }).call(blockEvent.blockNumber);
+
+  const price1 = new BigNumber(priceToken1);
+  const price2 = new BigNumber(priceToken2);
+  const exRate = price1.dividedBy(price2);
+
+  return generateFindings(exRate);
+}
 
 export default {
-  handleTransaction,
-  // handleBlock
+  handleBlock
 }
